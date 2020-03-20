8a667647f0ccad874b8ef0df1a2f90bd153051f9
David <mrrandolphjr@gmail.com>
2020-03-20 18:14:26 -0400

    Initial Commit

---
+++ RandomFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomFacts
+//
+//  Created by david randolph on 3/20/20.
+//  Copyright © 2020 david randolph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
